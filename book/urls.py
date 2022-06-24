d62542ec20cdf44fba4d02fffefc1e15107a545b
toshiba <yanshi.panda18@icloud.com>
2022-06-25 00:17:23 +0900

    django_restframework

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import BookList
+from .views import BookList,BookListAPI
 
 urlpatterns = [
     path('list/',BookList.as_view()),
+    path('api/',BookListAPI.as_view()),
 ]
--- book/views.py
+++ book/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import BookSerializer
 from .models import Book
 # Create your views here.
 
 class BookList(ListView):
     template_name = 'list.html'
     model = Book
+
+class BookListAPI(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
